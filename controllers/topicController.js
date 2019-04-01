65468799d6b8d862703397bf24f3da7e0326aedf
pfajar12 <email.pfajar@gmail.com>
2019-04-01 16:31:31 +0700

    add sort function to in-memory list

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -11,8 +11,11 @@ app.use(flash());
 
 // topics in-memory
 const topics = [
+    {topic : 'ini adalah test 2', votecount: 222},
     {topic : 'ini adalah test 1', votecount: 312},
-    {topic : 'ini adalah test 2', votecount: 222}
+    {topic : 'ini adalah test 3', votecount: 111},
+    {topic : 'ini adalah test 4', votecount: 444},
+    {topic : 'ini adalah test 5', votecount: 312},
 ];
 
 class TopicController{
@@ -20,7 +23,8 @@ class TopicController{
     static getAllData(req, res){
         var html = swig.compileFile('templates/index.html');
         var output = html({
-            topics  : topics
+            // sort topics by votecount descending
+            topics  : topics.sort((a, b) => (a.votecount > b.votecount) ? -1 : 1)
         });
         res.end(output);
     }
